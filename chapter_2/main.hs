f9ecbd1c5691daed52c79149a23d4417e7cbc989
Alexey Ershov <alex4rom@gmail.com>
2017-05-16 22:00:47 +0300

    Add ex

--- chapter_2/main.hs
+++ chapter_2/main.hs
@@ -28,5 +28,14 @@ module Chapter2 where
     -- (^) 10 $ 1 + 1 => (^) 10 (1 + 1)
     -- 2 ^ 2 * 4 ^ 5 + 1 => (2 ^ 2) * (4 ^ 5) + 1
 
-    
+    waxOn = 
+        x * 5
+        where
+        z = 7
+        x = y ^ 2
+        y = z + 8
+
+    triple x = x * 3
+
+    waxOff x = triple x
     
