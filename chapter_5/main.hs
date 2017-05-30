d5fed3f92d5c49e20d403e9262d39575d35d50c7
Alexey Ershov <alex4rom@gmail.com>
2017-05-30 11:01:07 +0300

    Update main.hs

--- chapter_5/main.hs
+++ chapter_5/main.hs
@@ -45,3 +45,32 @@ module Chapter5 where
 
 -- 3) let m :: a -> b -> b; m a b = b
 -- only one implementation, behaviour doesn't change when type of 'a' or 'b' changes
+
+-- Type inference
+
+-- 1) Type signature of general function
+-- (++) :: [a] -> [a] -> [a]
+-- How might that change when we apply
+-- it to the following value?
+-- myConcat x = x ++ " yo"
+-- It changes to myConcat :: [Char] -> [Char]
+
+-- 2) ? General function
+-- (*) :: Num a => a -> a -> a
+-- Applied to a value
+-- myMult x = (x / 3) * 5
+-- It changes to myMult :: Fractional a => a
+-- (correct answer Fractional a => a -> a)
+
+-- 3) take :: Int -> [a] -> [a]
+-- myTake x = take x "hey you"
+-- It changes to myTake :: Num a => [Char]
+-- (correct answer myTake :: Int -> [Char])
+
+-- 4) (>) :: Ord a => a -> a -> Bool
+-- myCom x = x > (length [1..10])
+-- It changes to myCom :: Int -> Bool
+
+-- 5) (<) :: Ord a => a -> a -> Bool
+-- myAlph x = x < 'z'
+-- It changes to myAlph :: Char -> Bool
