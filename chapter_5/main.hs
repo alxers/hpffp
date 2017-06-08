a2e145bf766092cca4b778e8099b0aadbcf73f04
Alexey Ershov <alex4rom@gmail.com>
2017-06-08 11:16:45 +0300

    Update ex.

--- chapter_5/main.hs
+++ chapter_5/main.hs
@@ -178,3 +178,12 @@ sndString x = x ++ " over the rainbow"
 sing = if (x > y) then fstString x or sndString y
     where x = "Singin"
           y = "Somewhere"
+          
+-- module Arith3Broken where
+-- main :: IO()
+-- main = do
+--     print (1 + 2)
+--     putStrLn "10"
+--     print (negate (- 1))
+--     print ((+) 0 blah)
+--     where blah = negate 1
