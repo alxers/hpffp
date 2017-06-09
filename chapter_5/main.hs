e04e12e332fca843994a9a1e91673d501b0e16cb
Alexey Ershov <alex4rom@gmail.com>
2017-06-09 11:15:02 +0300

    Create main.hs

--- chapter_5/main.hs
+++ chapter_5/main.hs
@@ -187,3 +187,35 @@ sing = if (x > y) then fstString x or sndString y
 --     print (negate (- 1))
 --     print ((+) 0 blah)
 --     where blah = negate 1
+
+
+-- Type-Kwon-Do
+-- 1) f :: Int -> String
+-- f = undefined
+-- g :: String -> Char
+-- g = undefined
+-- h :: Int -> Char
+-- h a = g (f a)
+
+-- 2) data A
+-- data B
+-- data C
+-- q :: A -> B
+-- q = undefined
+-- w :: B -> C
+-- w = undefined
+-- e :: A -> C
+-- e :: A -> C; e a = w (q a)
+
+-- 3) data X
+-- data Y
+-- data Z
+-- xz :: X -> Z
+-- xz = undefined
+-- yz :: Y -> Z
+-- yz = undefined
+-- xform :: (X, Y) -> (Z, Z)
+-- xform (x, y) = ((yz y), (yz y))
+
+-- 4) munge :: (x -> y) -> (y -> (w, z)) -> x -> w
+-- munge = ???
