ebb661794b6caa32dc57a9885e7b3922af2d16f1
Alexey Ershov <alex4rom@gmail.com>
2017-06-16 15:21:36 +0300

    Update main.hs

--- chapter_6/main.hs
+++ chapter_6/main.hs
@@ -4,6 +4,10 @@ module Chapter6 where
 
 -- Intermission: Exercises
 -- 1) max (length [1, 2, 3]) (length [8, 9, 10, 11, 12])
+-- works fine
 -- 2) compare (3 * 4) (3 * 5)
+-- works fine
 -- 3) compare "Julie" True
+-- doesn't work, diffent types
 -- 4) (5 + 3) > (3 + 6)
+-- works fine
